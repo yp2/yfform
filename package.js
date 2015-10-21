249ee4c52a9fccf2dfa811dfa723673f2e4cc161
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2015-10-21 23:23:19 +0200

    version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'yp2:yfform',
-    version: '0.0.2-2',
+    version: '0.0.2-3',
     summary: 'Forms for meteor',
     git: 'git push -u origin master',
     documentation: 'README.md'
